e417578a24e945896a25fe81692de0a357635a2c
massi-css <sebiha_massinissa@etu.univ-blida.dz>
2024-07-29 03:14:22 +0100

    added  intro and some effects

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ const field_of_studyInput = document.querySelector("#field-of-study");
 const sientificInterestsInput = document.querySelector("#interests");
 const experienceInput = document.querySelector("#experience");
 const why_joinInput = document.querySelector("#why-join");
-const grade_levelInputs = document.querySelectorAll('input[name="grade-level"]');
+const grade_levelInputs = document.querySelectorAll(
+  'input[name="grade-level"]'
+);
 const submitBtn = document.querySelector(".submit-btn[type='submit']");
 const nextBtn = document.querySelectorAll(".submit-btn[type='button']");
 const backbtn = document.querySelector(".back-btn");
@@ -16,9 +18,10 @@ const section2 = document.querySelector(".section-2");
 const section3 = document.querySelector(".section-3");
 const progress = document.querySelectorAll(".circle");
 const alert = document.querySelector(".alert");
+const sliderNavs = document.querySelectorAll(".slider-nav a");
+const showformBtn = document.querySelector(".show-form");
 
 const localStorage = window.localStorage;
-
 var sectionNumber = localStorage.getItem("sectionNumber") || 1;
 
 function toggleSection(sectionNumber) {
@@ -84,15 +87,13 @@ function loadFromLocalStorage() {
       if (input.value === data.grade_level) {
         input.checked = true;
       }
-    })
+    });
     toggleSection(sectionNumber);
     updateProgress();
   }
 }
 function backSection() {
-  // Check if both elements exist
   if (section1 && section2 && section3) {
-    // Toggle display style between 'none' and 'block'
     if (sectionNumber > 1) {
       sectionNumber--;
       toggleSection(sectionNumber);
@@ -105,9 +106,7 @@ function backSection() {
 }
 
 function nextSection() {
-  // Check if both elements exist
   if (section1 && section2 && section3) {
-    // Toggle display style between 'none' and 'block'
     if (sectionNumber < 3) {
       sectionNumber++;
       toggleSection(sectionNumber);
@@ -143,7 +142,8 @@ function checkSubmitedData() {
     experienceInput.value === "" ||
     why_joinInput.value === "" ||
     sientificInterestsInput.value === "" ||
-    selectedGrade === "" || selectedGrade === undefined
+    selectedGrade === "" ||
+    selectedGrade === undefined
   ) {
     alert.style.opacity = 1;
     alert.textContent = "Please fill in all fields !!";
@@ -155,6 +155,10 @@ function checkSubmitedData() {
 }
 
 async function sendData(data) {
+  const sidebg = document.querySelector(".side-bg");
+  const sidebgTitle = document.querySelector(".side-bg .title");
+  const sidebgSubTitle = document.querySelector(".side-bg .sub-title");
+
   console.log("Sending data to server");
   try {
     // const response = await axios.post("http://localhost:5000/register", data);
@@ -167,11 +171,123 @@ async function sendData(data) {
       body: JSON.stringify(data),
     });
     const message = await response.text();
-    console.log(message);
+    if (response.status === 201) {
+      alert.style.opacity = 1;
+      alert.style.backgroundColor = "green";
+      alert.textContent = message;
+      localStorage.clear();
+      document.querySelector(".form-content").reset();
+      // confetti();
+      sidebg.style.width = "100%";
+      sidebgTitle.innerHTML = "thank you for joining us";
+      sidebgSubTitle.innerHTML = "your data has been submited successfully ";
+      showformBtn.style.display = "none";
+
+      const colors = ["#bb0000", "#ffffff"];
+
+      (function frame() {
+        confetti({
+          particleCount: 2,
+          angle: 60,
+          spread: 55,
+          origin: { x: 0 },
+          colors: colors,
+        });
+        confetti({
+          particleCount: 2,
+          angle: 120,
+          spread: 55,
+          origin: { x: 1 },
+          colors: colors,
+        });
+        requestAnimationFrame(frame);
+      })();
+    }
   } catch (error) {
     console.error("Fetch error:", error);
   }
 }
+
+function smoothScrollSlide(e) {
+  e.preventDefault();
+
+  const targetId = this.getAttribute("href").substring(1);
+  const targetElement = document.getElementById(targetId);
+
+  targetElement.scrollIntoView({
+    behavior: "smooth",
+    block: "nearest",
+    inline: "start",
+  });
+}
+
+function setActiveNav(id) {
+  sliderNavs.forEach((nav) => {
+    nav.classList.remove("active");
+  });
+  sliderNavs.forEach((nav) => {
+    if (nav.getAttribute("href") === `#slider-${id}`) {
+      nav.classList.add("active");
+    }
+  });
+}
+
+function slideNext(id) {
+  if (id === sliderNavs.length + 1) {
+    return;
+  } else {
+    setTimeout(() => {
+      sliderNavs.forEach((nav) => {
+        if (nav.getAttribute("href") === `#slider-${id}`) {
+          nav.click();
+          setActiveNav(id);
+        }
+      });
+      slideNext(id + 1);
+    }, 7000);
+  }
+}
+
+function showForm() {
+  const sidebg = document.querySelector(".side-bg");
+  const slideContainer = document.querySelector(".slide-container");
+  const formContainer = document.querySelector(".form-container");
+  const sidebgTitle = document.querySelector(".side-bg .title");
+  const sidebgSubTitle = document.querySelector(".side-bg .sub-title");
+  if (showformBtn.innerHTML === "go back") {
+    sidebg.style.left = "60%";
+    sidebg.style.background =
+      "linear-gradient(to left, #202068 70%, transparent)";
+    slideContainer.style.visibility = "visible";
+    slideContainer.style.pointerEvents = "all";
+    formContainer.style.visibility = "hidden";
+    formContainer.style.opacity = 0;
+    showformBtn.innerHTML = "Join us";
+    sidebgTitle.innerHTML = "Hello there!";
+    sidebgSubTitle.innerHTML = "We are happy to see you here";
+  } else {
+    sidebg.style.left = "0%";
+    sidebg.style.background = "#202068";
+    slideContainer.style.visibility = "hidden";
+    slideContainer.style.pointerEvents = "none";
+    formContainer.style.visibility = "visible";
+    formContainer.style.opacity = 1;
+    showformBtn.innerHTML = "go back";
+    sidebgTitle.innerHTML =
+      "Registration Form for joining cscc club 2024 / 2025";
+    sidebgSubTitle.innerHTML =
+      "are you ready to join us? fill the next form and start your new journey with us ! we are waiting for you !!";
+  }
+}
+
+showformBtn.addEventListener("click", showForm);
+
+slideNext(1);
+setInterval(() => slideNext(1), 7000 * sliderNavs.length);
+
+sliderNavs.forEach((anchor) => {
+  anchor.addEventListener("click", smoothScrollSlide);
+});
 
 loadFromLocalStorage();
 
